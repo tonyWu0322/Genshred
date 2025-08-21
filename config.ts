34275dae2e0b06df97f7f1e1f0b7fc5e91beba8b
YuanYou <1175116595@qq.com>
2025-08-21 12:28:49 +0800

    服务器地址改为新服务器

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // 服务器配置文件
-export const SERVER_URL = process.env.PLASMO_PUBLIC_SERVER_URL || "http://47.98.207.81"; // 使用环境变量或默认值 
+export const SERVER_URL = process.env.PLASMO_PUBLIC_SERVER_URL || "http://8.159.133.45/"; // 使用环境变量或默认值 
 // 服务器基础URL，可以根据需要修改
 // export const SERVER_URL = process.env.PLASMO_PUBLIC_SERVER_URL || "http://localhost:5000"; // 使用环境变量或默认值 
 // 本地开发URL
